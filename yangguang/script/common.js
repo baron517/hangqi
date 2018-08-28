d46c96d54976703b663f15b517fed84f32e4abbf
baron517 <450175465@qq.com>
2018-08-29 07:35:00 +0800

    a

---
+++ yangguang/script/common.js
@@ -0,0 +1,6 @@
+
+
+$("body").append("<div style='height:160px;width:100px;background: #000;position: absolute;z-index:100000;top:0;left:0;opacity: 0.2;' onclick='window.location.reload();'>刷新</div>");
+
+
+
